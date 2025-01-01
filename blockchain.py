15dc8958ee91df0f97762e1195de4c24ef0df5b3
vikiaseschool <vyhnalekv@ceskolipska.cz>
2025-01-01 13:06:30 +0100

    Update blockchain.py
    - smazani ProofOfWork verifikace, moc narocna na priklad
    - prevdeni na flask app pro vyukovy program

--- blockchain.py
+++ blockchain.py
@@ -1,32 +1,25 @@
-import hashlib
+from flask import Flask, jsonify, request, render_template
+from ecdsa import SigningKey, VerifyingKey, SECP256k1
 import json
-import time
+import hashlib
 
+app = Flask(__name__, static_url_path='', static_folder='static')
 
 class Block:
-    def __init__(self, index, transactions, previous_hash, timestamp=None):
+    def __init__(self, index, transactions, previous_hash):
         self.index = index
         self.transactions = transactions
         self.previous_hash = previous_hash
-        self.timestamp = timestamp or time.time()
-        self.nonce = 0
         self.hash = self.calculate_hash()
 
     def calculate_hash(self):
-        block_data = f"{self.index}{json.dumps(self.transactions)}{self.previous_hash}{self.timestamp}{self.nonce}"
+        block_data = f"{self.index}{json.dumps(self.transactions)}{self.previous_hash}"
         return hashlib.sha256(block_data.encode()).hexdigest()
 
-    def mine_block(self, difficulty):
-        while not self.hash.startswith("0" * difficulty):
-            self.nonce += 1
-            self.hash = self.calculate_hash()
-
 class Blockchain:
-    def __init__(self, difficulty=4):
+    def __init__(self):
         self.chain = [self.create_genesis_block()]
-        self.difficulty = difficulty
         self.pending_transactions = []
-        self.mining_reward = 50
 
     def create_genesis_block(self):
         return Block(0, [], "0")
@@ -34,30 +27,82 @@ class Blockchain:
     def get_latest_block(self):
         return self.chain[-1]
 
-    def mine_pending_transactions(self, miner_address):
-        block = Block(len(self.chain), self.pending_transactions, self.get_latest_block().hash)
-        block.mine_block(self.difficulty)
-        self.chain.append(block)
-        print(f"Block mined: {block.hash}")
-
-        self.pending_transactions = [{
-            'from': None,
-            'to': miner_address,
-            'amount': self.mining_reward
-        }]
-
-    def create_transaction(self, transaction):
+    def add_transaction(self, transaction):
         self.pending_transactions.append(transaction)
 
-    def is_chain_valid(self):
-        for i in range(1, len(self.chain)):
-            current = self.chain[i]
-            previous = self.chain[i - 1]
+    def add_block(self):
+        if not self.pending_transactions:
+            return "No transactions to add"
+        new_block = Block(len(self.chain), self.pending_transactions, self.get_latest_block().hash)
+        self.chain.append(new_block)
+        self.pending_transactions = []
+        return f"Block {new_block.index} added with hash: {new_block.hash}"
 
-            if current.hash != current.calculate_hash():
-                return False
+class Wallet:
+    def __init__(self):
+        self.private_key = SigningKey.generate(curve=SECP256k1)
+        self.public_key = self.private_key.verifying_key
 
-            if current.previous_hash != previous.hash:
-                return False
+    def create_transaction(self, recipient, amount):
+        transaction = {
+            "sender": self.public_key.to_string().hex(),
+            "recipient": recipient,
+            "amount": amount
+        }
+        transaction["signature"] = self.private_key.sign(json.dumps(transaction, sort_keys=True).encode()).hex()
+        return transaction
 
-        return True
+blockchain = Blockchain()
+wallets = []
+
+@app.route('/')
+def serve_index():
+    return render_template('index.html')
+
+@app.route('/wallet/new', methods=['GET'])
+def create_wallet():
+    wallet = Wallet()
+    wallets.append(wallet)
+    return jsonify({
+        "private_key": wallet.private_key.to_string().hex(),
+        "public_key": wallet.public_key.to_string().hex()
+    })
+
+@app.route('/wallets', methods=['GET'])
+def get_wallets():
+    wallets_data = [{"public_key": wallet.public_key.to_string().hex()} for wallet in wallets]
+    return jsonify(wallets_data)
+
+@app.route('/transaction/new', methods=['POST'])
+def create_transaction():
+    data = request.json
+    sender = data.get('sender')
+    recipient = data.get('recipient')
+    amount = data.get('amount')
+    signature = data.get('signature')
+    transaction = {
+        "sender": sender,
+        "recipient": recipient,
+        "amount": amount,
+        "signature": signature
+    }
+    blockchain.add_transaction(transaction)
+    return jsonify({"message": "Transaction added to pending transactions"})
+
+@app.route('/block/new', methods=['POST'])
+def add_block():
+    result = blockchain.add_block()
+    return jsonify({"message": result})
+
+@app.route('/blockchain', methods=['GET'])
+def get_blockchain():
+    chain_data = [{
+        "index": block.index,
+        "transactions": block.transactions,
+        "previous_hash": block.previous_hash,
+        "hash": block.hash
+    } for block in blockchain.chain]
+    return jsonify(chain_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
